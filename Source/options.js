9abad70edd9af0e86c8339d61308b0c6be19de98
Kumar Uttpal <uttpal25993@gmail.com>
2016-03-02 00:34:34 +0530

    Issue #9 added more measurment units

--- Source/options.js
+++ Source/options.js
@@ -1,28 +1,49 @@
-
 function save_options() {
-  var emission = document.getElementById("emission");
-  var carbonEmission = emission.value;
-  localStorage["carbonEmission"] = carbonEmission;
+    var emission = document.getElementById("emission");
+    var unit = document.getElementById("unit");
+    var carbonEmission = emission.value;
+    var emissionUnit = unit.value;
+    switch (emissionUnit) {
+        case "kg/km":
+            carbonEmission = 1000 * carbonEmission;
+            break;
+        case "g/mi":
+            carbonEmission = 0.621371 * carbonEmission;
+            break;
+        case "kg/mi":
+            carbonEmission = 621.371 * carbonEmission;
+            break;
+        case "ltr/km":
+            carbonEmission = 2320 * carbonEmission;
+            break;
+        case "gal/km":
+            carbonEmission = 8782.155 * carbonEmission;
+            break;
+        default:
+            emissionUnit = emission.value;
+    }
+    localStorage["carbonEmission"] = carbonEmission;
 
-  // Update status to let user know options were saved.
-  var status = document.getElementById("save-message");
-  status.innerHTML = "Saved!";
-  setTimeout(function() {
-    status.innerHTML = "";
-  }, 750);
+    // Update status to let user know options were saved.
+    var status = document.getElementById("save-message");
+    status.innerHTML = "Saved!";
+    setTimeout(function() {
+        status.innerHTML = "";
+    }, 750);
 }
 
-function S(key) { return localStorage[key]; }
+function S(key) {
+    return localStorage[key]; }
 
 function restore_options() {
-  var emission = document.getElementById("emission");
-  emission.setAttribute('value', S("carbonEmission"));
+    var emission = document.getElementById("emission");
+    emission.setAttribute('value', S("carbonEmission"));
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('save-button').addEventListener('click', save_options);
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('save-button').addEventListener('click', save_options);
 });
 
-window.onload = restore_options ;
+window.onload = restore_options;
 
 google_analytics('UA-1471148-11');
