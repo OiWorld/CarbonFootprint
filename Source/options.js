522b01de04b23b90906a7ee7f9441541b6c29009
Rohan Goel <goelrohan6@gmail.com>
2016-03-04 16:59:15 +0530

    updated options.js for calculating cost

--- Source/options.js
+++ Source/options.js
@@ -3,6 +3,15 @@ function saveOptions() {
   var emission = document.getElementById('emission');
   var carbonEmission = emission.value;
   localStorage['carbonEmission'] = carbonEmission;
+  
+  var average = document.getElementById('average');
+  var vehicleAverage = average.value;
+  localStorage['vehicleAverage'] = vehicleAverage;
+  
+  var fuelPrice = document.getElementById('price');
+  var vehicleFuelPrice = fuelPrice.value;
+  localStorage['vehicleFuelPrice'] = vehicleFuelPrice;
+
 
   // Update status to let user know options were saved.
   var status = document.getElementById('save-message');
@@ -17,6 +26,13 @@ function S(key) { return localStorage[key]; }
 function restoreOptions() {
   var emission = document.getElementById('emission');
   emission.setAttribute('value', S('carbonEmission'));
+  
+  var fuelPrice = document.getElementById('price');
+  fuelPrice.setAttribute('value', S('vehicleFuelPrice'));
+  
+  var vehicleAverage = document.getElementById('average');
+  vehicleAverage.setAttribute('value', S('vehicleAverage'));
+  
 }
 
 document.addEventListener('DOMContentLoaded', function() {
