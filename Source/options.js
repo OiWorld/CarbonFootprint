86e0f567e06dadc79d3bfcb3bd221d0235f84c48
goelrohan6 <goelrohan6@gmail.com>
2016-03-07 00:42:34 +0530

    removed extra logs

--- Source/options.js
+++ Source/options.js
@@ -7,34 +7,33 @@ to avoid redundancy caused by making all combinations
  
 $('#saveButton1').click(function() {
     var fuel = document.getElementById('fuelInput1');
-        fuelValue = fuel.value;
-        distance = document.getElementById('distanceInput1');
-        distanceValue = distance.value;
-        input = document.getElementById('fuelConsumptionInput');
-        inputValue = input.value;
-        status = document.getElementById('save-message');
+    var fuelValue = fuel.value;
+    var distance = document.getElementById('distanceInput1');
+    var distanceValue = distance.value;
+    var input = document.getElementById('fuelConsumptionInput');
+    var inputValue = input.value;
+    var status = document.getElementById('saveMessage');
     status.innerHTML = 'Saved!';
     switch (fuelValue) {
         case "l":
             if (distance == "km") {
-                console.log("l/km case");
                 var conversionConstant = 2328;
-                    fuelConsumption = conversionConstant * inputValue;
+                var fuelConsumption = conversionConstant * inputValue;
                 localStorage['carbonEmission'] = fuelConsumption;
             } else {
                 var conversionConstant = 2328;
-                    fuelConsumption = conversionConstant * inputValue;
+                var fuelConsumption = conversionConstant * inputValue;
                 localStorage['carbonEmission'] = fuelConsumption;
             }
             break;
         case "g":
             if (distance == "km") {
                 var conversionConstant = 8782.155;
-                    fuelConsumption = conversionConstant * inputValue;
+                var fuelConsumption = conversionConstant * inputValue;
                 localStorage['carbonEmission'] = fuelConsumption;
             } else {
                 var conversionConstant = 5456.97;
-                    fuelConsumption = 1 / inputValue;
+                var fuelConsumption = 1 / inputValue;
                 fuelConsumption = conversionConstant * fuelConsumption;
             }
 
@@ -44,24 +43,24 @@ $('#saveButton1').click(function() {
 
 $('#saveButton2').click(function() {
     var unit = document.getElementById('distanceFuelInput2');
-        unitValue = unit.value;
-        input = document.getElementById('fuelEfficiencyInput');
-        inputValue = input.value;
-        status = document.getElementById('save-message-2');
+    var unitValue = unit.value;
+    var input = document.getElementById('fuelEfficiencyInput');
+    var inputValue = input.value;
+    var status = document.getElementById('saveMessage2');
     status.innerHTML = 'Saved!';
     if (unitValue == "km/l") {
         var conversionConstant = 2328;
-            fuelEfficiency = 1 / inputValue;
+        var fuelEfficiency = 1 / inputValue;
         fuelEfficiency = conversionConstant * fuelEfficiency;
         localStorage['carbonEmission'] = fuelEfficiency;
     } else if (unitValue == "km/gl") {
         var conversionConstant = 8782.155;
-            fuelEfficiency = 1 / inputValue;
+        var fuelEfficiency = 1 / inputValue;
         fuelEfficiency = conversionConstant * fuelEfficiency;
         localStorage['carbonEmission'] = fuelEfficiency;
     } else {
         var conversionConstant = 5456.97;
-            fuelEfficiency = conversionConstant * inputValue;
+        var fuelEfficiency = conversionConstant * inputValue;
         localStorage['carbonEmission'] = fuelEfficiency;
     }
 });
@@ -69,14 +68,13 @@ $('#saveButton2').click(function() {
 $('#saveButton3').click(function() {
 
     var fuel = document.getElementById('fuelInput3');
-        fuelValue = fuel.value;
-        distance = document.getElementById('distanceInput3');
-        distanceValue = distance.value;
-        input = document.getElementById('co2EmissionInput');
-        inputValue = input.value;
-        status = document.getElementById('save-message-3');
+    var fuelValue = fuel.value;
+    var distance = document.getElementById('distanceInput3');
+    var distanceValue = distance.value;
+    var input = document.getElementById('co2EmissionInput');
+    var inputValue = input.value;
+    var status = document.getElementById('saveMessage3');
     status.innerHTML = 'Saved!';
-    console.log(inputValue);
     switch (fuelValue) {
         case "g":
             if (distance == "km") {
@@ -84,19 +82,19 @@ $('#saveButton3').click(function() {
                 localStorage['carbonEmission'] = co2Emission;
             } else {
                 var conversionConstant = 0.621371;
-                    co2Emission = conversionConstant * inputValue;
+                var co2Emission = conversionConstant * inputValue;
                 localStorage['carbonEmission'] = co2Emission;
             }
             break;
         case "kg":
             if (distance == "km") {
                 var conversionConstant = 1000;
-                    co2Emission = conversionConstant * inputValue;
+                var co2Emission = conversionConstant * inputValue;
                 localStorage['carbonEmission'] = co2Emission;
                 localStorage['massFlag'] = 2;
             } else {
                 var conversionConstant = 621.371;
-                    co2Emission = conversionConstant * inputValue;
+                var co2Emission = conversionConstant * inputValue;
                 localStorage['carbonEmission'] = co2Emission;
                 localStorage['massFlag'] = 2;
 
@@ -105,13 +103,13 @@ $('#saveButton3').click(function() {
         case "p":
             if (distance == "km") {
                 var conversionConstant = 453.592;
-                    co2Emission = conversionConstant * inputValue;
+                var co2Emission = conversionConstant * inputValue;
                 localStorage['carbonEmission'] = co2Emission;
                 localStorage['massFlag'] = 1;
 
             } else {
                 var conversionConstant = 281.849;
-                    co2Emission = conversionConstant * inputValue;
+                var co2Emission = conversionConstant * inputValue;
                 localStorage['carbonEmission'] = co2Emission;
                 localStorage['massFlag'] = 1;
             }
@@ -124,7 +122,6 @@ function S(key) {
     return localStorage[key]; }
 
 function restoreOptions() {
-    console.log("function restore options");
 
     var fuelConsumption = document.getElementById('fuelConsumption');
     fuelConsumption.setAttribute('value', S('carbonEmission'));
