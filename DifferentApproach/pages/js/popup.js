1b1a8dfd2fb0fc5c3ac2b5eca3885e47165e4102
Kolya Opahle <kolyaopahle@gmail.com>
2016-05-27 18:18:02 +0200

    removed rest of old(new) folder structure

--- DifferentApproach/pages/js/popup.js
+++
@@ -1,9 +0,0 @@
-function openTab(element) {
-  console.log("yo",element);
-  window.close();
-  chrome.tabs.create({url: chrome.extension.getURL('pages/'+element+'.html')});
-}
-
-$('.tab').click(function(){
-  openTab($(this).data('tab'));
-});
