57e55710288a3c81b3402645b7b2fb19a6d55ce6
heychirag <heychiragarora@gmail.com>
2016-06-03 01:56:05 +0530

    Added Currency Selection

--- Source/pages/js/options.js
+++ Source/pages/js/options.js
@@ -1,5 +1,7 @@
 var optionsData;
 
+var currencyCodes = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BOV","BRL","BSD","BTN","BWP","BYR","BZD","CAD","CDF","CHE","CHF","CHW","CLF","CLP","CNY","COP","COU","CRC","CUP","CVE","CYP","CZK","DJF","DKK","DOP","DZD","EEK","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","INR","IQD","IRR","ISK","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LTL","LVL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MTL","MUR","MVR","MWK","MXN","MXV","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SKK","SLL","SOS","SRD","STD","SYP","SZL","THB","TJS","TMM","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","USN","USS","UYU","UZS","VEB","VND","VUV","WST","XAF","XAG","XAU","XBA","XBB","XBC","XBD","XCD","XDR","XFO","XFU","XOF","XPD","XPF","XPT","XTS","XXX","YER","ZAR","ZMK","ZWD"];
+
 //function to switch between tabs and open respective inputs
 /*function openEmissionInput()
 {
@@ -234,6 +236,13 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
     
+    for(var i = 0; i < currencyCodes.length; i++) {
+      $('#currency-codes')
+        .append($('<option></option>')
+                .html(currencyCodes[i])
+                .val(i)
+               )
+    }
     // Added multiple language support. replaces text with user language
     for(var i=0;i< $('[data-language]').length;++i) {
       $($('[data-language]')[i]).html(chrome.i18n.getMessage($($('[data-language]')[i]).data('language')))
