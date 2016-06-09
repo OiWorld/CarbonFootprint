9ebab0b4d246320b7e56cafdbd04ceb936957356
heychirag <heychiragarora@gmail.com>
2016-06-09 16:23:15 +0530

    swapped input fields, moved fuel types to JSON

--- Source/pages/js/options.js
+++ Source/pages/js/options.js
@@ -1,6 +1,12 @@
-var optionsData;
+var optionsData,fuels;
+
+$.ajaxSetup({
+  async: false
+});
 
 var currencyCodes = ["AED","AFN","ALL","AMD","ANG","AOA","ARS","AUD","AWG","AZN","BAM","BBD","BDT","BGN","BHD","BIF","BMD","BND","BOB","BOV","BRL","BSD","BTN","BWP","BYR","BZD","CAD","CDF","CHE","CHF","CHW","CLF","CLP","CNY","COP","COU","CRC","CUP","CVE","CYP","CZK","DJF","DKK","DOP","DZD","EEK","EGP","ERN","ETB","EUR","FJD","FKP","GBP","GEL","GHS","GIP","GMD","GNF","GTQ","GYD","HKD","HNL","HRK","HTG","HUF","IDR","ILS","INR","IQD","IRR","ISK","JMD","JOD","JPY","KES","KGS","KHR","KMF","KPW","KRW","KWD","KYD","KZT","LAK","LBP","LKR","LRD","LSL","LTL","LVL","LYD","MAD","MDL","MGA","MKD","MMK","MNT","MOP","MRO","MTL","MUR","MVR","MWK","MXN","MXV","MYR","MZN","NAD","NGN","NIO","NOK","NPR","NZD","OMR","PAB","PEN","PGK","PHP","PKR","PLN","PYG","QAR","RON","RSD","RUB","RWF","SAR","SBD","SCR","SDG","SEK","SGD","SHP","SKK","SLL","SOS","SRD","STD","SYP","SZL","THB","TJS","TMM","TND","TOP","TRY","TTD","TWD","TZS","UAH","UGX","USD","USN","USS","UYU","UZS","VEB","VND","VUV","WST","XAF","XAG","XAU","XBA","XBB","XBC","XBD","XCD","XDR","XFO","XFU","XOF","XPD","XPF","XPT","XTS","XXX","YER","ZAR","ZMK","ZWD"];
+
+
 
 function switchtab(){
   $('.tab-button.selected').removeClass('selected');
@@ -161,7 +167,7 @@ var saveLocation = function(){
 function loadSavedData() {
   //restore only if options html was saved once
   if(optionsData.has('init')) {
-    $('#fuel-type').val(optionsData.get('fuelType'));
+    $('[id="fuel-type"]').val(optionsData.get('fuelType'));
     $('#fuel-cost').val(optionsData.get('fuelCost')['value']);
     $('#currency-codes').val(optionsData.get('fuelCost')['curr']);
     $('#distance-value').val(optionsData.get('distance'));    
@@ -196,6 +202,7 @@ function toggleInputSource(elem){
       }else{
         toggleInputType($('.inputType>:checkbox:checked'));
       }
+      $('.cost-fields :eq(0)').hide();
     }
     else{
       $elem.prop('checked',true);
@@ -206,6 +213,7 @@ function toggleInputSource(elem){
       $('#by-fuel-consumption').prop('checked',false);
       $('.by-fuel input,.by-fuel select').prop('disabled',true);
       $('.by-co input,.by-co select').prop('disabled',false);
+      $('.cost-fields :eq(0)').show();
     }
     else{
       elem.prop('checked',true);
@@ -254,10 +262,10 @@ function toggleInputType(elem){
 
 function toggleTravelCost(elem){
   if(elem.prop('checked')){
-    $('#fuel-cost,#fuel-cost-volume,#currency-codes').prop('disabled',false);
+    $('.cost-fields select,.cost-fields input').prop('disabled',false);
   }
   else{
-    $('#fuel-cost,#fuel-cost-volume,#currency-codes').prop('disabled',true);
+    $('.cost-fields select,.cost-fields input').prop('disabled',true);
   }
 }
 
@@ -290,6 +298,9 @@ function toggleUnits(elem){
   }
 }
 
+function mirrorFuelValues(elem){
+  $('[id="fuel-type"]').val(elem.prop('value'));
+}
 
 $(document).bind('DOMContentLoaded', function () {
   optionsData = new StorageManager('calculationObject', function() {
@@ -301,29 +312,34 @@ $(document).bind('DOMContentLoaded', function () {
     $('.tab-button').on('click',switchtab);
 
 		$('#by-fuel-consumption,#direct-co-emission').on('click',function(){
-      toggleInputSource(jQuery(this));
+      toggleInputSource($(this));
     });
     
     $('#fuel-efficiency,#fuel-consumed-per,#custom-values').on('click',function(){
-      toggleInputType(jQuery(this));
+      toggleInputType($(this));
     });
 
     $('#display-travel-cost').on('click',function(){
-      toggleTravelCost(jQuery(this));
+      toggleTravelCost($(this));
     });
     
     $('#metric,#uscustomary').on('click',function(){
-      toggleUnits(jQuery(this));
+      toggleUnits($(this));
     });
+
+    $('[id="fuel-type"]').on('change',function(){
+      mirrorFuelValues($(this));
+    });
+
     /**
      * Prevents adding Hyphen(-) in the input field.
      */
-    /*$('#distance-value,#emission,#fuel-value,#fuel-cost')
+    $('#distance-value,#emission,#fuel-value,#fuel-cost')
       .bind('keypress',function(evtmin){
         if(evtmin.which === 45){
           evtmin.preventDefault();
         }
-      });*/
+      });
 
     for(var i=0;i<currencyCodes.length;i++) {
       $('#currency-codes')
@@ -332,7 +348,20 @@ $(document).bind('DOMContentLoaded', function () {
                 .val(i)
                );
     }
- 
+
+    $.getJSON("/core/resources/fuels.json",function(response){
+      fuels=response.fuels;
+    });
+
+    for(var i=0;i<fuels.length;i++) {
+      $('[id="fuel-type"]')
+        .append($('<option></option>')
+                .html(fuels[i].fuel)
+                .val(i)
+                .attr('data-language',fuels[i].langKey)
+               );
+    }
+    
     // Added multiple language support. replaces text with user language
     for(var i=0;i<$('[data-language]').length;i++) {
       $($('[data-language]')[i])
@@ -341,7 +370,6 @@ $(document).bind('DOMContentLoaded', function () {
                                     )
              );
     }
-    
     loadSavedData();
   });
 });
