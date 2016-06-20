4ae117e1098f8d2f9cc613320b2e4c05fdc0ac84
heychirag <heychiragarora@gmail.com>
2016-06-21 00:18:34 +0530

    passing more files through gjslint

--- Source/core/settings/ChromeSettingsProvider.js
+++ Source/core/settings/ChromeSettingsProvider.js
@@ -1,58 +1,102 @@
 /**
- * Created by Kolpa on 22.03.2016.
- * Contributed by PrateekGupta1509
+ * loads extension settings
+ * @author Kolpa (Kolya Opahle)
+ * @author PrateekGupta1509 (Prateek Gupta)
+ * @author heychirag (Chirag Arora)
  */
-var ChromeSettingsProvider = function (cb) {
-    var self = this;
 
-    self.usingDefaultListeners = [];
+/**
+ * ChromeSettingsProvider namespace
+ * @constructor
+ * @param {function} cb
+ */
 
-    chrome.storage.sync.get('calculationObject', function (settings) {
-        if (settings.calculationObject)
-            self.settings = settings.calculationObject;
-        else
-            self.settings = {};
-        console.log(self.settings);
-        cb(self);
-    });
+var ChromeSettingsProvider = function(cb) {
+  var self = this;
+  self.usingDefaultListeners = [];
+  chrome.storage.sync.get('calculationObject', function(settings) {
+    if (settings.calculationObject)
+      self.settings = settings.calculationObject;
+    else
+      self.settings = {};
+    console.log(self.settings);
+    cb(self);
+  });
 };
 
-ChromeSettingsProvider.prototype.addUsingDefaultListener = function (listener) {
+/**
+ * CarbonFootprintCore namespace.
+ * @param {function} listener
+ */
+
+ChromeSettingsProvider.prototype.addUsingDefaultListener =
+  function(listener) {
     this.usingDefaultListeners.push(listener);
 };
 
-ChromeSettingsProvider.prototype.has = function (key) {
-    return key in this.settings;
+/**
+ * checks if given 'key' is present
+ * @param {string} key
+ * @return {boolean}
+ */
+
+ChromeSettingsProvider.prototype.has = function(key) {
+  return key in this.settings;
 };
 
-ChromeSettingsProvider.prototype.get = function (key, def) {
-    if (this.has(key))
-        return this.settings[key];
+/**
+ * returns value stored in given 'key' if it is present
+ * else returns 'def' value
+ * @param {string} key
+ * @param {number} def
+ * @return {number}
+ */
 
-    for (var i in this.usingDefaultListeners) {
-        var listener = this.usingDefaultListeners[i];
-        this.usingDefaultListeners.splice(i, 1);
-
-        listener();
-    }
-
-    return def;
+ChromeSettingsProvider.prototype.get = function(key, def) {
+  if (this.has(key))
+    return this.settings[key];
+  for (var i in this.usingDefaultListeners) {
+    var listener = this.usingDefaultListeners[i];
+    this.usingDefaultListeners.splice(i, 1);
+    listener();
+  }
+  return def;
 };
+
+/**
+ * returns carbon emission rate
+ * @return {number}
+ */
 
 ChromeSettingsProvider.prototype.getCarbonEmission = function() {
-    return this.get('emissionRate', 217);
+  return this.get('emissionRate', 217);
 };
+
+/**
+ * returns carbon emission unit
+ * @return {number}
+ */
 
 ChromeSettingsProvider.prototype.getCarbonEmissionUnit = function() {
-    return this.get('emissionDisplayUnit','g');
+  return this.get('emissionDisplayUnit', 'g');
 };
 
-ChromeSettingsProvider.prototype.getTravelRate = function () {
-    return this.get('travelRate', 30);
+/**
+ * returns travel rate
+ * @return {number}
+ */
+
+ChromeSettingsProvider.prototype.getTravelRate = function() {
+  return this.get('travelRate', 30);
 };
 
-ChromeSettingsProvider.prototype.showTravelCost = function () {
-    return this.get('showTravelCost', false);
+/**
+ * whether to show travel cost
+ * @return {boolean}
+ */
+
+ChromeSettingsProvider.prototype.showTravelCost = function() {
+  return this.get('showTravelCost', false);
 };
 
 var SettingsProvider = ChromeSettingsProvider;
--- Source/pages/js/datePatch.js
+++ Source/pages/js/datePatch.js
@@ -1,29 +1,30 @@
-//patching date functions because original java sucks
-
-Date.isLeapYear = function (year) { 
-    return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)); 
+Date.isLeapYear = function(year) {
+  return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0));
 };
 
-Date.getDaysInMonth = function (year, month) {
-    return [31, (Date.isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
+Date.getDaysInMonth = function(year, month) {
+  return [
+    31, (Date.isLeapYear(year) ? 29 : 28),
+    31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+  ][month];
 };
 
-Date.prototype.isLeapYear = function () { 
-    return Date.isLeapYear(this.getFullYear()); 
+Date.prototype.isLeapYear = function() {
+  return Date.isLeapYear(this.getFullYear());
 };
 
-Date.prototype.getDaysInMonth = function () { 
-    return Date.getDaysInMonth(this.getFullYear(), this.getMonth());
+Date.prototype.getDaysInMonth = function() {
+  return Date.getDaysInMonth(this.getFullYear(), this.getMonth());
 };
 
-Date.prototype.addMonths = function (value) {
-    var n = this.getDate();
-    this.setDate(1);
-    this.setMonth(this.getMonth() + value);
-    this.setDate(Math.min(n, this.getDaysInMonth()));
-    return this;
+Date.prototype.addMonths = function(value) {
+  var n = this.getDate();
+  this.setDate(1);
+  this.setMonth(this.getMonth() + value);
+  this.setDate(Math.min(n, this.getDaysInMonth()));
+  return this;
 };
 
-Date.prototype.addYears = function (value) {
-	return this.addMonths(value * 12);
+Date.prototype.addYears = function(value) {
+  return this.addMonths(value * 12);
 };
