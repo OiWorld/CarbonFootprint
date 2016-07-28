91141d70d89db516dc8b2cbd8dac283d6fcd9f10
PrateekGupta1509 <prateek.g1509@gmail.com>
2016-07-29 02:11:36 +0530

    added sedentary adult calorie consumption per day

--- Source/pages/js/caloriesCalc.js
+++ Source/pages/js/caloriesCalc.js
@@ -55,8 +55,6 @@ $('#caloriesFormSubmit').click(function(){
 	}
 	var caloriesBurnt = caloriesPerHour * time;
 	
-
-
 	$('#outputCalories').html('Congratulations! You have burnt <b>' + caloriesBurnt + ' Calories</b>.<br>'
 		+ ' Moreover, a sedentary women require 1,600 to 2,000 calories a day, while'
 		+ ' sedentary men need about 2,000 to 2,600 calories daily to maintain their current weight. ');
