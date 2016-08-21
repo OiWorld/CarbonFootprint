5e769283085bd55afedd8d7733a9360aa59d16d6
Kolya Opahle <kolyaopahle@gmail.com>
2016-08-21 22:03:17 +0200

    whoops thats not needed anymore

--- Source/Firefox/pages/js/popupPanel.js
+++ Source/Firefox/pages/js/popupPanel.js
@@ -1,9 +1,3 @@
-var myScript = "window.addEventListener('click', function(event) {" +
-               "  var t = event.target;" +
-               "  if (t.nodeName == 'A')" +
-               "    self.port.emit('click-link', t.toString());" +
-               "}, false);"
-
 window.addEventListener('click', function(ev) {
 	var target = ev.target;
 	if (target.dataset.tab) {
