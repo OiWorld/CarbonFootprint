434012d2bb516b9554cd3135e140d423494616e5
Vaibhav <vaib.sharma44@gmail.com>
2017-06-07 05:57:05 +0000

    Flights

---
+++ Source/Core/core/initFlight.js
@@ -0,0 +1,13 @@
+var flightManager = new FlightManager();
+var core = new FlightsFootprintCore();
+var target = document.getElementsByTagName("body")[0],
+    observer = new MutationObserver(function() {
+      console.log('Observing!');
+      flightManager.update();
+    });
+observer.observe(target, {
+  attributes: true,
+  childList: false,
+  characterData: true,
+  subtree: flightManager.subtree
+});
