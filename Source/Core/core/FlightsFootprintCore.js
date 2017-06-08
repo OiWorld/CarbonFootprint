1aad26a772c9f8d6f51bc710de9637209740d7e7
Vaibhav Sharma <vaib.sharma44@gmail.com>
2017-06-08 14:29:31 +0530

    kayak is working

--- Source/Core/core/FlightsFootprintCore.js
+++ Source/Core/core/FlightsFootprintCore.js
@@ -19,9 +19,9 @@ FlightsFootprintCore.prototype.getCoordinates = function(list){
   //console.log(core.airportsData);
   for(var x = 0, i = list.length; x < i; x++){
     list[x].departCoordinates = core.airportsData[list[x].depart];
-    list[x].arriveCoordinates = core.airportsData[list[x].arrive];
+      list[x].arriveCoordinates = core.airportsData[list[x].arrive];
+      list[x].stopCoordinatesNew = [];
       if(list[x].stops.length){
-          list[x].stopCoordinatesNew = [];
           for(var y=0;y<list[x].stops.length;y++){
               list[x].stopCoordinatesNew.push(core.airportsData[list[x].stops[y]]);
           }
@@ -36,7 +36,7 @@ FlightsFootprintCore.prototype.getTotalDistance = function(processedList){
         processedList[x].distance = 0;
         console.log(processedList[x]);
         console.log(processedList[x].stopCoordinatesNew)
-      if(processedList[x].stopCoordinatesNew){
+      if(processedList[x].stopCoordinatesNew.length>0){
           noOfStops = processedList[x].stopCoordinatesNew.length;
           console.log(noOfStops);
       processedList[x].distance += this.getDistance(processedList[x].departCoordinates.lat, processedList[x].departCoordinates.lon,
@@ -108,8 +108,8 @@ FlightsFootprintCore.prototype.createMark = function(depart,arrive){
       var e = document.createElement('div');
      //knowMoreUrl = this.helper.getFilePath('pages/knowMore.html');
     e.setAttribute("id", "carbon-footprint-label");
-    e.innerHTML = '<a href=\'\' target=\'_blank\' title=\'Depart :' +
-        depart + 'kg of C02e per person.\nReturn :'+ arrive + 'kg of CO2e per person.' + '\' class=\'carbon\' id=\'carbon\'>' + 'CarbonFootprints ' +
+    e.innerHTML = "<a href=\'\' target=\'_blank\' title=\'Outbound :" +
+        depart + "kg of CO" +"2".sub()+  "e per person.\nReturn :"+ arrive + "kg of CO<sub>2</sub>"+"e per person." + '\' class=\'carbon\' id=\'carbon\'>' +parseInt(arrive+depart)+ " kg of CO" +"2".sub()+ " per person" +
       // question mark icon using svg
       '<svg id="quest_mark_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 92 92"><path d="M45.4 0C20 0.3-0.3 21.2 0 46.6c0.3 25.4 21.2 45.7 46.6 45.4 25.4-0.3 45.7-21.2 45.4-46.6C91.7 20 70.8-0.3 45.4 0zM45.3 74l-0.3 0c-3.9-0.1-6.7-3-6.6-6.9 0.1-3.8 2.9-6.5 6.7-6.5l0.2 0c4 0.1 6.7 3 6.6 6.9C51.9 71.3 49.1 74 45.3 74zM61.7 41.3c-0.9 1.3-2.9 2.9-5.5 4.9l-2.8 1.9c-1.5 1.2-2.5 2.3-2.8 3.4 -0.3 0.9-0.4 1.1-0.4 2.9l0 0.5H39.4l0-0.9c0.1-3.7 0.2-5.9 1.8-7.7 2.4-2.8 7.8-6.3 8-6.4 0.8-0.6 1.4-1.2 1.9-1.9 1.1-1.6 1.6-2.8 1.6-4 0-1.7-0.5-3.2-1.5-4.6 -0.9-1.3-2.7-2-5.3-2 -2.6 0-4.3 0.8-5.4 2.5 -1.1 1.7-1.6 3.5-1.6 5.4v0.5H27.9l0-0.5c0.3-6.8 2.7-11.6 7.2-14.5C37.9 18.9 41.4 18 45.5 18c5.3 0 9.9 1.3 13.4 3.9 3.6 2.6 5.4 6.5 5.4 11.6C64.4 36.3 63.5 38.9 61.7 41.3z" /></svg>';
     e.querySelector('a').addEventListener('click', function(e) {
