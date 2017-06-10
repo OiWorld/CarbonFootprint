44d2d3328b01375d10d78a68285855ef50e2d379
Nakul Havelia <nakulhavelia@gmail.com>
2017-06-10 21:43:54 +0530

    added support for tgv-lyria.com

---
+++ Source/Core/core/trains/tgv-lyria.js
@@ -0,0 +1,38 @@
+var lyriaManager = function(){
+  this.subtree = true;
+  this.dataSource = "sncf"; //select one of the emission information sources from trainEmissions.json
+  this.stations = {
+    arrive: "",
+    depart: ""
+  };
+  this.MODE = "lyria"; // constant, the type of train on this website is only "lyria"
+};
+
+lyriaManager.prototype.geocodeStations = function(){
+  var stations = document.getElementsByTagName("h2")[0].innerText;
+  console.log(stations);
+  this.stations.depart = stations.split(" > ")[0];
+  this.stations.arrive = stations.split(" > ")[1];
+  console.log(this.stations.depart + " ->  " + this.stations.arrive);
+  if(core.distance === 0 && this.stations.depart && this.stations.arrive){  //Check if geocode never happened for current stations, proceed if not
+    var toGeocode = [this.stations.depart, this.stations.arrive];
+    core.geocode(toGeocode);
+  }
+};
+
+lyriaManager.prototype.insertInDom = function(emission){
+  console.log(emission);
+  var element = document.getElementsByTagName("h2")[0];
+  if(element.getElementsByClassName('carbon').length === 0){
+    element.appendChild(emission);
+  }
+};
+
+lyriaManager.prototype.update = function(){
+  this.geocodeStations();
+  if(core.distance > 1){ //Check if station have alredy been geocoded
+    this.insertInDom(core.getEmission(["lyria"])); //There is only 1 type of train
+  }
+};
+
+var TrainManager = lyriaManager;
