1ac0ce1f450d34fbeeece136145be52eea3ed328
Vaibhav Sharma <vaib.sharma44@gmail.com>
2017-07-08 14:12:44 +0530

    code refactor

--- Source/Core/core/initFlight.js
+++ Source/Core/core/initFlight.js
@@ -2,7 +2,7 @@ var flightManager = new FlightManager();
 var core = new FlightsFootprintCore();
 
 /**
- * Function for Updating the DOM
+ * Function for Updating the DOM for flight websites
  */
 
 var flightsUpdate = function(){
@@ -16,14 +16,23 @@ var flightsUpdate = function(){
     console.log(processedList);
 };
 
+/**
+ * Structure to observe for changes
+ */
+
 var target = document.getElementsByTagName("body")[0],
     observer = new MutationObserver(function() {
-      console.log('Observing!');
-      flightsUpdate();
+        console.log('Observing!');
+        flightsUpdate();
     });
+
+/**
+ * Setting the properties for mutation Observer
+ */
+
 observer.observe(target, {
-  attributes: true,
-  childList: true,
-  characterData: true,
-  subtree: flightManager.subtree
+    attributes: true,
+    childList: true,
+    characterData: true,
+    subtree: flightManager.subtree
 });
