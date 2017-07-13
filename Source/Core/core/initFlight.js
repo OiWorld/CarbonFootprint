7a074ed848d087ba42c6d9a944d4b60e8dd5a33c
Vaibhav Sharma <vaib.sharma44@gmail.com>
2017-07-14 04:28:23 +0530

    a middleware for permission

--- Source/Core/core/initFlight.js
+++ Source/Core/core/initFlight.js
@@ -1,12 +1,15 @@
-var flightManager = new FlightManager();
-var core = new FlightsFootprintCore();
-var a = new inform();
-
 /**
- * Function for Updating the DOM
+ * Initialises the extension when flights webpage is open
+ * @author vaibsharma (Vaibhav Sharma)
  */
 
-var flightsUpdate = function(){
+var flightManager = new FlightManager();
+var core = new FlightsFootprintCore();
+var Inform = new inform();
+var flightsInit = function(){
+};
+
+flightsInit.prototype.update = function(){
     var processedList = flightManager.getList();
     if(core.airplanesData && core.airportsData){
         processedList = flightManager.getCoordinates(processedList);
@@ -17,12 +20,11 @@ var flightsUpdate = function(){
     console.log(processedList);
 };
 
+var serviceManager = new flightsInit();
 var target = document.getElementsByTagName("body")[0],
     observer = new MutationObserver(function() {
         console.log('Observing!');
-        if(a.permission()){
-            flightsUpdate();
-        }
+        Inform.permission(serviceManager);
     });
 
 observer.observe(target, {
