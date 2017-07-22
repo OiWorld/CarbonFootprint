a97e52ff29f8f35e6536e41c45a36da014afde69
Nakul Havelia <nakulhavelia@gmail.com>
2017-07-22 21:29:47 +0530

    integrated sentry.io

--- Source/Core/core/validator/validatorServer.js
+++ Source/Core/core/validator/validatorServer.js
@@ -2,16 +2,21 @@
  * Server namespace.
  * @constructor
  */
-var Server = function(){
-
-};
+ var Server = function(){
+   Raven.config('https://e21c0743051946a899ed8d6680d1c58c@sentry.io/185232').install();
+ };
 
 /**
  * Send error report to server
  * @param {string} website
  * @param {string} error
  */
- 
-Server.prototype.error = function(website, error){
-  console.log(error + " on " + website);
-};
+
+ Server.prototype.error = function(website, error){
+   console.error(error + " on " + website);
+   Raven.captureMessage(error, {
+     extra: {
+       website: website,
+     }
+   });
+ };
